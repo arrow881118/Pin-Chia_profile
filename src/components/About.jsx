1af28d269e8fdefa571f21142153cc4f40051d86
name <Email>
2023-04-24 15:58:31 +0800

    pop1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Portrait from '../assets/portrait.png';
+import Popup from './AboutPop';
 
 const About = () => {
+
+
+  const [showMyPopup, setShowMyPopup] = useState(false)
+
+  const handleOnClose = () => setShowMyPopup(false)
+
   return (
-    <section className='w-full bg-white py-10 px-4 ' id='about'>
+    <section className='overscroll-contain w-full bg-white py-10 px-4 ' id='about'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 space-x-7 justify-center'>
-      {/* <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'> */}
+        {/* <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'> */}
         <div className='max-w-[400px] mx-auto flex flex-col'>
-        {/* <div className='w-[400px] mx-auto flex flex-col'> */}
+          {/* <div className='w-[400px] mx-auto flex flex-col'> */}
           <img className='mx-auto my-auto rounded-lg' src={Portrait} alt='/' />
           <h1 className='mx-auto my-auto text-slate-900 font-serif'>LIFE PHILOSOPHY</h1>
-          <p translation="Amanda go go go!"className='w-[300px] h-[24px] before:content-["子曰：「參乎！吾道一以貫之。」"] hover:before:content-[attr(translation)] mx-auto my-2 text-[#3664e1] md:text-xl sm:text font-serif text-center'></p>
+          <p translation="Amanda go go go!" className='w-[300px] h-[24px] before:content-["子曰：「參乎！吾道一以貫之。」"] hover:before:content-[attr(translation)] mx-auto my-2 text-[#3664e1] md:text-xl sm:text font-serif text-center'></p>
         </div>
         {/* <div className=' mx-auto flex flex-col'> */}
         <div className='max-w-[400px] mx-auto flex flex-col'>
@@ -30,8 +37,11 @@ const About = () => {
           <p className='mx-auto text-gray-400 font-sans text-xs'>
             <br />Click the button below for the story that brings me here
           </p>
-          <button className='mx-auto bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 py-3'>Learn more about me</button>
+          <button onClick={() => setShowMyPopup(true)} className='mx-auto bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 py-3'>
+            Learn more about me
+          </button>
         </div>
+        <Popup onClose={handleOnClose} visible={showMyPopup} />
       </div>
     </section>
   );
