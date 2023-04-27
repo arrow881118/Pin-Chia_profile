4f4cc97c8cf8bc2e5130c1a3a9e2ee5892fbecc3
name <Email>
2023-04-28 00:23:24 +0800

    art

--- src/components/Hobby.jsx
+++ src/components/Hobby.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import Motor from '../assets/motorcycle.png'
 import Bouldering from '../assets/bouldering.png'
+import goldfish from '../assets/goldfish.jpg'
 import Motorpop from './motorpop';
 import Boulderingpop from './boulderingpop';
+import Paintingpop from './paintingpop';
 
 const Cards = () => {
     const [mshowMyPopup, msetShowMyPopup] = useState(false)
@@ -10,6 +12,9 @@ const Cards = () => {
 
     const [bshowMyPopup, bsetShowMyPopup] = useState(false)
     const bhandleOnClose = () => bsetShowMyPopup(false)
+
+    const [pshowMyPopup, psetShowMyPopup] = useState(false)
+    const phandleOnClose = () => psetShowMyPopup(false)
 
     return (
         <div className='w-full py-10 px-4 bg-white text-black grid justify-start-center' id='hobby'>
@@ -22,7 +27,7 @@ const Cards = () => {
                     <div className='text-center font-medium'>
                         <p className='mx-8 text-xs pt-8 text-left'>Why</p>
                         <p className=' border-b mx-8 '>Responsible for vehicle condition<br></br>before hit on the road</p>
-                        <p className='mx-8 text-xs pt-8 text-left'>Benefit</p>
+                        <p className='mx-8 text-xs pt-8 text-left'>Influence</p>
                         <p className=' border-b mx-8'>Self-learning skill improved</p>
                     </div>
                     <button onClick={() => msetShowMyPopup(true)}  className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>read more</button>
@@ -30,29 +35,30 @@ const Cards = () => {
                 <div className='w-full shadow-xl bg-white flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
                     <img className='rounded-lg w-40 h-32 mx-auto mt-[-3rem] bg-transparent' src={Bouldering} alt="/" />
                     <h2 className='text-3xl font-bold text-center py-5'>Bouldering</h2>
-                    <p className='text-gray-600 text-center text-xl font-medium'>Improve strength and flexibility</p>
+                    <p className='text-gray-600 text-center text-xl font-medium'>Improve Strength and Flexibility</p>
                     <div className='text-center font-medium'>
                         <p className='mx-8 text-xs pt-8 text-left'>Why</p>
                         <p className=' border-b mx-8  text-center'>Solving bouldering puzzles feels<br></br>like I am one of the Mario bros</p>
-                        <p className='mx-8 text-xs pt-8 text-left'>Benefit</p>
+                        <p className='mx-8 text-xs pt-8 text-left'>Influence</p>
                         <p className=' border-b mx-8 text-center'>Closer to be 8th HOKAGE</p>
                     </div>
                     <button onClick={() => bsetShowMyPopup(true)} className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>read more</button>
                 </div>
                 <div className='w-full shadow-xl bg-white flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
-                    <img className='rounded-lg w-40 h-32 mx-auto mt-[-3rem] bg-white' src={Bouldering} alt="/" />
-                    <h2 className='text-3xl font-bold text-center py-5'>Single User</h2>
-                    <p className='text-gray-600 text-center text-xl font-medium'>$149</p>
+                    <img className='rounded-lg w-40 h-32 mx-auto mt-[-3rem] bg-white' src={goldfish} alt="/" />
+                    <h2 className='text-3xl font-bold text-center py-5'>Painting</h2>
+                    <p className='text-gray-600 text-center text-xl font-medium'>To be the One</p>
                     <div className='text-center font-medium'>
                         <p className='mx-8 text-xs pt-8 text-left'>Why</p>
-                        <p className=' border-b mx-8  text-centert'>Feels like a ninja</p>
-                        <p className='mx-8 text-xs pt-8 text-left'>Control device</p>
-                        <p className=' border-b mx-8 text-center'>Logitech G29/Keyboard</p>
+                        <p className=' border-b mx-8  text-centert'>Gave me the opportunity to be bold on <br></br>colors, dreaming and imagination</p>
+                        <p className='mx-8 text-xs pt-8 text-left'>Influence</p>
+                        <p className=' border-b mx-8 text-center'></p>
                     </div>
-                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>read more</button>
+                    <button onClick={() => psetShowMyPopup(true)} className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>read more</button>
                 </div>
                 <Motorpop onClose={mhandleOnClose} visible={mshowMyPopup} />
                 <Boulderingpop onClose={bhandleOnClose} visible={bshowMyPopup} />
+                <Paintingpop onClose={phandleOnClose} visible={pshowMyPopup} />
             </div>
         </div>
     );
---
+++ src/components/paintingpop.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+import adidas from '../assets/adidas.jpg';
+import egypt from '../assets/egypt.jpg';
+import Lily from '../assets/Calla Lily.png';
+import sun from '../assets/sun.jpg';
+import Strelitzia from '../assets/Strelitzia reginae.jpg';
+import Strelitzia2 from '../assets/Strelitzia reginae2.jpg';
+import portrait from '../assets/portrait.jpg';
+import chicken from '../assets/chicken.jpg';
+import dragon from '../assets/dragon.jpg';
+import Lily2 from '../assets/Calla Lily2.jpg';
+import goldfish from '../assets/goldfish.jpg';
+import peacock from '../assets/peacock.jpg';
+
+
+
+
+export default function popup({ visible, onClose }) {
+
+    const handleOnClose = (e) => {
+        if (e.target.id === "container") {
+            onClose()
+        }
+
+    }
+
+    if (!visible) return null;
+
+    return (
+        <div id="container" onClick={handleOnClose} className='fixed -inset-7 mx-auto my-auto bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
+            <div className=' overflow-auto overscroll-contain max-h-[650px] max-w-[1000px] bg-white p-2 rounded m-14'>
+                <h1 className='border-b-2 border-gray-500 text-2xl py-4 text-slate-600 capitalize text-center font-serif'>
+                    Art Class.
+                </h1>
+                <section className='py-6 mx-auto grid border-b-2 border-gray-200'>
+                    <p className='mx-auto my-auto text-slate-600 font-serif  text-justify'>
+                        According to my parent, they sent me to learned painting since I was four. After study in junior high, there was no time for art class anymore, so the last work of mime end in 2012.<br></br>
+                        Most of my work is preserved by my dad. Although I don't paint as much as I used to be, art class still affect a alot in different ways to my life.
+                        And I'll never forget that teacher told me to keeps imagination, creative and always be the "one".
+                    </p>
+                </section>
+                <h1 className='border-b-2 border-gray-500 text-2xl py-4 text-slate-600 capitalize text-center font-serif'>
+                    2007.
+                </h1>
+                <section className='py-6 mx-auto grid md:grid-cols-2 my-auto item-center border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={adidas} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-justify'>
+                        Adidas Shoes<br></br>It was made of plaster and I made it right after the my dad bought me our first pair of father and son shoes.
+                    </p>
+                </section>
+                <section className='py-6 mx-auto grid md:grid-cols-2 my-auto item-center border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={egypt} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-justify'>
+                        Egypt Gods.<br></br>I painted the whole plastic board then scratched the color off to draw.
+                    </p>
+                </section>
+                <h1 className='border-b-2 border-gray-500 text-2xl py-4 text-slate-600 capitalize text-center font-serif'>
+                    2008.
+                </h1>
+                <section className='py-6 mx-auto grid md:grid-cols-2 my-auto item-center border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={Lily} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-justify'>
+                        Moth Orchid. <br></br>I love that I could still tell the theme of it without see the label.
+                    </p>
+                </section>
+                <h1 className='border-b-2 border-gray-500 text-2xl py-4 text-slate-600 capitalize text-center font-serif'>
+                    2009.
+                </h1>
+                <section className='py-6 mx-auto grid md:grid-cols-2 border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={sun} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'>
+                        Vincent Van Gogh Imitations.<br></br>You can still see the painting was made by hundred of lines.
+                    </p>
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'></p>
+                </section>
+                <h1 className='border-b-2 border-gray-500 text-2xl py-4 text-slate-600 capitalize text-center font-serif'>
+                    2010.
+                </h1>
+                <section className='py-6 mx-auto grid md:grid-cols-2 border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={Strelitzia} alt='/' />
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={Strelitzia2} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'>
+                        Bird of Paradise Flower.<br></br>
+                    </p>
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'>
+                        I always remember I had drawn a unusual plant.
+                    </p>
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'></p>
+                </section>
+                <section className='py-6 mx-auto grid md:grid-cols-2 border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={portrait} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'>
+                        Self Portrait.<br></br>What kinds of sick person draw himself like this......, I thought I was happy back then.
+                    </p>
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'></p>
+                </section>
+                <h1 className='border-b-2 border-gray-500 text-2xl py-4 text-slate-600 capitalize text-center font-serif'>
+                    2011.
+                </h1>
+                <section className='py-6 mx-auto grid md:grid-cols-2 border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={chicken} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'>
+                        Cockfighting.<br></br>I watched lot of cockfight that week, that when I realized butcher are not the only one could make things bloody.
+                    </p>
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'></p>
+                </section>
+                <section className='py-6 mx-auto grid md:grid-cols-2 border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={dragon} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'>
+                        Dragon.<br></br>I love how the letter looks like, it means dragon and had been used thousand years ago. The one we use now is "龍".
+                    </p>
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'></p>
+                </section>
+                <section className='py-6 mx-auto grid md:grid-cols-2 border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={Lily2} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'>
+                        Calla Lily<br></br>This is my favorite plant.
+                    </p>
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'></p>
+                </section>
+                <h1 className='border-b-2 border-gray-500 text-2xl py-4 text-slate-600 capitalize text-center font-serif'>
+                    2012.
+                </h1>
+                <section className='py-6 mx-auto grid md:grid-cols-2 border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={goldfish} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'>
+                        Goldfish.
+                    </p>
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'></p>
+                </section>
+                <section className='py-6 mx-auto grid md:grid-cols-2 border-b-2 border-gray-200'>
+                    <img className='mx-auto my-auto w-[200px] grid rounded-lg' src={peacock} alt='/' />
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'>
+                        Peacock.
+                    </p>
+                    <p className='mx-auto my-auto text-slate-600 py-2 font-serif text-center'></p>
+                </section>
+
+            </div>
+        </div >
+    )
+}
